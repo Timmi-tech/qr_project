325f1d9201b8e708557546fb5431717edc62cee2
Odufowokan ayotomiwa <102088546+Timmi-tech@users.noreply.github.com>
2024-09-03 19:53:39 +0100

    added gunicorn

--- qr_app/views.py
+++ qr_app/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .models import QRCode
 
+@csrf_exempt
 def scan_code(request):
     return render(request, 'scan_code.html')
 
+@csrf_exempt
 def admit_code(request):
     if request.method == 'POST':
         code = request.POST.get('code', None)
